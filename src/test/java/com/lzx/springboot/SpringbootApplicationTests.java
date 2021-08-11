9f573dddfe416be10effac7e6af51904c2422612
lizhengxin <lizhengxin.lzx@bytedance.com>
2021-08-11 10:01:45 +0800

    zhushi1

--- src/test/java/com/lzx/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/lzx/springboot/SpringbootApplicationTests.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringbootApplicationTests {
 
     public static void main(String[] args) {
+        // zhushi1
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1, 10, TimeUnit.SECONDS,
             new ArrayBlockingQueue<>(10),
             new RejectedExecutionHandler() {
